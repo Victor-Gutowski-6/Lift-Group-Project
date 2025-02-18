016d2a533dde36c29d2bc327d98121e4da8f8db2
Victor-Gutowski-6 <vjgutowski06@gmail.com@gmail.com>
2025-02-18 21:00:53 +0000

    Most of the main algorithm, few changes to be made from the to do list.

--- My_Algorithm.py
+++ My_Algorithm.py
@@ -1,28 +1,32 @@
 import time
 import json
+import sys
 from algorithms import (
     printfloors,    
-    look,
-    scan,
     SafetyChecks,
-    AskDirection,
+    AskDirection, # Need to make a feature that lets people request the lift on the floor.
     init_heap,
     insert,
     remove,
     peek,
-    build_heap
+    build_heap,
+    moving
 )
 
 
 #The floors start from floor 1 and go to floor 10. There is no Ground floor.
 # We assume the weight limit of the lift is far above any achievable amount.
+# We assume each person only chooses ONE floor
 
-dictionary = {}
-heap = []
+heap = init_heap()
 floor = 1
 people = 0
 
-def My_Algorithm(dictionary,heap,floor,peopleF): # the F is just to make it easier to tell which the function variable is.
+def My_Algorithm(heap,people): # the F is just to make it easier to tell which the function variable is.
+    # Performs safety checks
+    while SafetyChecks(people) == False:
+        sys.exit() # Terminates the function if not safe
+    
     printfloors()
     
     while True:
@@ -35,17 +39,41 @@ def My_Algorithm(dictionary,heap,floor,peopleF): # the F is just to make it easi
         except ValueError:
             print("Invalid input. Please enter a floor.")
 
+    # Increases the priority of every unvisited floor by 1 
+    for sub in heap:
+        sub[0] += 1
+
     
-    peopleF = peopleF + 1
-    name = "person" + str(peopleF) #giving the person a 'name' to keep track of in the dictionary??
+    people = people + 1 #NEED TO REMEMBER TO RETURN GLOBAL PEOPLE COUNT AT THE END 
 
-    dictionary[exit] = 0 #Every value will increase with every stop the lift makes that isnt the exit value.
-    print(dictionary)
-     
-
-
-
+    if any(sub[1] == exit for sub in heap):
+        repeats = []
+    else:
+        insert(heap,0,exit) # [priority,exit floor]
+    print(heap)
 
 
 
-My_Algorithm(dictionary,heap,floor,people)
+    return heap,people
+
+
+
+heap, people = My_Algorithm(heap,people)
+
+
+
+
+
+
+# This will control which floor the lift is on, people exiting, and the amoint of people in the lift
+def My_Algorithm_exits(heap,floor,people): #floor is just the current floor the lift is on.
+    while heap:
+        heap = build_heap(heap)
+        next_floor = remove(heap)
+        floor = next_floor
+
+        moving(floor,heap)
+        people -= 1 # Need to handle multiple people leaving the lift
+        print(f"{people} remaining in the lift.")
+    
+    return heap,floor,people
--- algorithms.py
+++ algorithms.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 def printfloors():
     with open('info.json', 'r') as file:
@@ -12,6 +13,7 @@ def printfloors():
     print("\n")
 
 # This will go to the next requested floor using the look algorithm
+#Need to add a way to remove the item from the queue
 def look(floor,queue):
     while queue[0] != floor and queue != []:
         
@@ -76,14 +78,13 @@ def AskDirection():
 
 
 def init_heap():
-    heap = []
-    return heap
+    return []
 
 
 def heapify_up(heap,index):
     while index > 0:
         parent = (index - 1) // 2
-        if heap [parent] > heap [index]:
+        if heap [parent][0] < heap [index][0]:
 
             x = heap[parent]
             heap[parent] = heap[index]
@@ -97,32 +98,32 @@ def heapify_up(heap,index):
 def heapify_down(heap,index):
     size = len(heap)
     while True:
-        smallest = index
+        largest = index 
         L_child = 2*index + 1
         R_child = 2*index + 2
 
-        if L_child < size and heap[L_child] < heap[smallest]:
-            smallest = L_child
+        if L_child < size and heap[L_child][0] > heap[largest][0]: 
+            largest = L_child
 
-        if R_child < size and heap[R_child] < heap[smallest]:
-            smallest = R_child
+        if R_child < size and heap[R_child][0] > heap[largest][0]: 
+            largest = R_child
 
-        if smallest != index:
+        if largest != index: 
 
             x = heap[index]
-            heap[index] = heap[smallest]
-            heap[smallest] = x
+            heap[index] = heap[largest]
+            heap[largest] = x 
 
-            index = smallest
+            index = largest 
 
         else:
             break
 
 # the priortiy will be input looking something like dictionary[value[i]]
-def insert(heap,priority_level): # The priority will be a dictionary value; the floor will be the key
-    heap.append(priority_level)
-    heapify_up(heap, heap[-1])  #There may be some issues with calling the function, look into this if any errors occur 
-
+def insert(heap,priority_level,floor): # The priority will be a dictionary value; the floor will be the key
+    heap.append([priority_level,floor])
+    heapify_up(heap, len(heap)-1)  #There may be some issues with calling the function, look into this if any errors occur     # Fix indexing issue
+ 
 
 def is_empty(heap):
     return len(heap) == 0 
@@ -151,6 +152,13 @@ def peek(heap):
 
 
 def build_heap(heap): # Note: this isnt a heap yet, just an unsorted array
-    for i in range(len(heap)//2 ,-1,0): # 0 could be -1?
+    for i in range(len(heap)//2 ,-1,-1): # 0 could be -1?
         heapify_down(heap,i)
     return heap
+
+# Creates a time delay when moving between floors
+def moving(floor,heap):
+    for f in range (floor,heap[1]):
+        time.sleep(3) # 3 seconds to move between floors
+        floor += 1
+        print("The lift is now on floor: ", floor)
